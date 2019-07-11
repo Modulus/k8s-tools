c8a0801180546f9aa923d9e090525ebf87bb4c38
Modulus <thejohnskauge@gmail.com>
2019-07-11 22:28:47 +0200

    Fixed failing import

--- integration/aws/route53/test_dns_zone.py
+++ integration/aws/route53/test_dns_zone.py
@@ -1,9 +1,9 @@
 import pytest
 
-import aws.route53.dns_zone as dns_zone
+import aws.route53.dns_zone as aws
 
 @pytest.mark.slow
 def test_get_dnz_zone():
-    zone = dns_zone.get_hosted_zone("aws5.tv2.no", private=False)
+    zone = aws.get_hosted_zone("aws5.tv2.no", private=False)
 
     assert zone != None
