bd118480a015ddfb068b4c0da41324090c8a17c5
John Sigvald Skauge <johnsigvald.skauge@tv2.no>
2019-07-11 14:36:06 +0200

    Started on kubernetes stuff

--- utils/helpers.py
+++ utils/helpers.py
@@ -6,7 +6,7 @@ def  extract_zone_id(id:str=None) -> str:
 	"""Hosted zone id has the format /hostedzone/Z40FIOTQIPYWL9"""
 	keyword = "/hostedzone/"
 	if keyword in id:
-		logger.info(f"Found {keyword} in {id} stripping id")
+		logger.info(f"Found {keyword} in {id} stripping it down")
 		return id[len(keyword):len(id)]
 	logger.info(f"{keyword} not found returning {id}")
 	return id
